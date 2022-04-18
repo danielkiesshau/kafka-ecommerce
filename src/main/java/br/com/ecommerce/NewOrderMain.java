6bf252d2c8ffccb15576c1fb2cfd2bbaefe15f8f
Daniel Kiesshau <danielkiesshau@outlook.com>
2022-04-17 23:08:54 -0300

    refactor(ecommerce): key passed to producer record
    to be able to work with multiple partitions

--- src/main/java/br/com/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/NewOrderMain.java
@@ -8,13 +8,12 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-
-
 
         Callback callback = (data, ex) -> {
             if (ex != null) {
@@ -25,11 +24,12 @@ public class NewOrderMain {
             System.out.println("send succeeded " + data.topic() + " ::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
         };
 
+        var key = UUID.randomUUID().toString();
         var value = "13214324, 56473, 989475102398";
-        var orderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        var orderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
 
         var email = "Thank you for your order! We are processing your order";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
 
         producer.send(orderRecord, callback).get();
         producer.send(emailRecord, callback).get();
