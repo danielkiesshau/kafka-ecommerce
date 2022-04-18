61e8df9daed0b68f221df1cf41ba43dc3ca9238a
Daniel Kiesshau <danielkiesshau@outlook.com>
2022-04-17 23:45:52 -0300

    refactor(ecommerce): dispatcher class

--- src/main/java/br/com/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/ecommerce/FraudDetectorService.java
@@ -6,9 +6,15 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudDetectorService = new FraudDetectorService();
-        var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudDetectorService::parse);
-
-        service.run();
+        try(
+            var service = new KafkaService(
+                FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudDetectorService::parse
+            )
+        ) {
+            service.run();
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record) {
--- src/main/java/br/com/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/NewOrderMain.java
@@ -1,48 +1,19 @@
 package br.com.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Map;
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        try(var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var order = "13214324, 56473, 989475102398";
+                var email = "Thank you for your order! We are processing your order";
 
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
-
-            System.out.println("send succeeded " + data.topic() + " ::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
-
-        var key = UUID.randomUUID().toString();
-        var value = "13214324, 56473, 989475102398";
-        var orderRecord = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-
-        var email = "Thank you for your order! We are processing your order";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-
-        producer.send(orderRecord, callback).get();
-        producer.send(emailRecord, callback).get();
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        // string to bytes config
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
+        }
     }
 }
