230c6fa15a8a4c3c6662ce1cbdf214f8f06ffcd3
Daniel Kiesshau <danielkiesshau@outlook.com>
2022-04-22 11:25:07 -0300

    refactor(ecommerce): email new order service

--- service-email-new-order/src/main/java/br/com/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/ecommerce/EmailNewOrderService.java
@@ -1,33 +1,19 @@
 package br.com.ecommerce;
 
-import br.com.ecommerce.consumer.KafkaService;
 import br.com.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
     private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<>();
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try(
-            var service = new KafkaService(
-                EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of()
-            )
-        ) {
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         var message = record.value();
 
         System.out.println("------------------------------------");
@@ -45,4 +31,13 @@ public class EmailNewOrderService {
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), email, id);
     }
 
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
+    }
 }
